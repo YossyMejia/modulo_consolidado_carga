486a957ada7d145894a8df8dd8c90e24c62c88ca
Your Name <you@example.com>
2020-05-14 18:07:39 -0600

    añadir datos

---
+++ README.md
@@ -0,0 +1,14 @@
+Procodi-Module
+=========
+
+Módulo para la empresa Procodi
+
+Modo de uso:
+1. Instalar el módulo
+2. Que no despliegue ningun error
+3. Revisar las vista y que esten los campos especificos
+  
+<h2>Authors</h2>
+<h4>Original Author and Development Lead</h4>
+<h5>Carlos Monge Molina</h5>
+<h5>Rolando</h5>
---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Consolidated Cargo',
+    'version': '12.0.2.0.0',
+    'author': 'JackDevelopers.com',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/charlit02390',
+    'category': 'Product',
+    'description':
+        '''
+        Modulo para Procodi
+        ''',
+    'depends': ['stock'],
+    'data': [
+        'views/consolidated_cargo_view.xml',
+        'views/menu_views.xml'
+    ],
+    'installable': True,
+}
